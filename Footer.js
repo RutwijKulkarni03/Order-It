d1606da6fb76d057559ac996c985fe0aeb42529b
Rutwij Kulkarni <118127489+RutwijKulkarni03@users.noreply.github.com>
2023-10-20 10:57:19 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="text-center">
+          Food Delivery Website - 2023-2024, All Rights Reserved..
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
